e340ff11800128d0b957c3fbeea7d7acc0cefa66
JPS915 <104226724+JPS915-xdarked@users.noreply.github.com>
2022-05-19 22:35:02 +0100

    Update index.js
    added more commands

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+client.on('messageCreate', msg => {
+  if (msg.content === '.help') {
+    msg.reply('```commands, .help . ping .bread .drink .breadgod .connection```');
+  }
+});
+
+
+
 client.on('messageCreate', msg => {
   if (msg.content === '.ping') {
     msg.reply('pong');
@@ -19,9 +28,25 @@ client.on('messageCreate', msg => {
   });
 
   client.on('messageCreate', msg => {
-    if (msg.content === '.stone') {
-      msg.reply(':emoji_15:');
+    if (msg.content === '.drink') {
+      msg.reply('Ok, i have :beer:');
     }
   });
 
-client.login('token');
+  client.on('messageCreate', msg => {
+    if (msg.content === '.breadgod') {
+      msg.reply('Im beginning to feel like a Bread God, Bread God All my people from the front to the back nod, back nod Now, who thinks their arms are long enough to slap box, slap box? They said I eat Bread like a robot, so call me Bread-bot.');
+    }
+  });
+
+client.on('messageCreate', message => {
+  if (message.content === '.connection') {  
+    message.channel.send(`latency is ${Date.now() - message.createdTimestamp}ms. Discord API latency is ${Math.round(client.ws.ping)}ms`);
+  }
+});
+
+
+
+client.login('bottokenhere').then(() => {
+  client.user.setPresence({ activities: [{ name: '.help | https://discord.gg/eAEWHD9E4c', type: 'PLAYING' }], status: 'online' });
+});
